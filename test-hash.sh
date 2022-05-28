308f947c71ff21a06967a0c5acee101149a2a105
YoavGutmanUCSD <ygutman@ucsd.edu>
2022-05-27 22:19:15 -0700

    You

--- test-hash.sh
+++ test-hash.sh
@@ -1,7 +1,7 @@
 classpath="src/:lib/hamcrest-core-1.3.jar:lib/junit-4.12.jar:classes/:."
-hash_map="src/FileReader.java src/DefaultMap.java src/MyHashMap.java src/MaxHeap.java src/MyHashMapTest.java"
+hash_map="src/DefaultMap.java src/MyHashMap.java src/MaxHeap.java src/MyHashMapTest.java"
 file_student="src/FileReader.java src/Student.java"
-javac -cp ${classpath} -d classes/ $file_student $hash_map
+javac -cp ${classpath} -d classes/ $hash_map # $file_student
 java -cp $classpath MyHashMapTest
 # javac -cp ${classpath} -d classes/ src/ArrayListADT.java
 # javac -cp ${classpath} -d classes/ src/CircularArrayList.java

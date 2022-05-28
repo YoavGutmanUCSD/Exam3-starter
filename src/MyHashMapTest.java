2605fa31e0b934610d71a916ffd14229205668d8
YoavGutmanUCSD <ygutman@ucsd.edu>
2022-05-27 22:09:24 -0700

    First commit

--- src/MyHashMapTest.java
+++ src/MyHashMapTest.java
@@ -4,21 +4,44 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import java.util.*;
 import org.junit.*;
 
 public class MyHashMapTest {
 	
 	private FileReader filereader;
 	private DefaultMap<Integer, Student> testMap; // use this for basic tests
+        private MaxHeap<String, Integer> testHeap;
 
-	@Before
-	public void setUp() {
-		filereader = new FileReader("src/input.txt");
-	}
+	// @Before
+	// public void setUp() {
+	// 	filereader = new FileReader("src/input.txt");
+	// }
+        @Before
+        public void initialize() {
+            Comparator comp = new TestComp();
+            testHeap = new MaxHeap<String, Integer>(15, comp);
+        }
 
+        @Test
+        public void testAddHeap(){
+            testHeap.add("a", 5);
+            testHeap.add("b", 7);
+            testHeap.add("c", 1);
+            assertEquals("[a, b, c]", testHeap.toString());
+        }
 
 
 	//Write testcase for checking max score of 2 sections
 
 	
+}
+
+class TestComp implements Comparator<HeapEntry<String, Integer>> {
+    @Override
+    public int compare(HeapEntry e, HeapEntry f){
+        String key1 = (String) e.getKey();
+        String key2 = (String) f.getKey();
+        return key1.compareTo(key2);
+    }
 }
---
+++ test-hash.sh
@@ -0,0 +1,19 @@
+classpath="src/:lib/hamcrest-core-1.3.jar:lib/junit-4.12.jar:classes/:."
+hash_map="src/FileReader.java src/DefaultMap.java src/MyHashMap.java src/MaxHeap.java src/MyHashMapTest.java"
+file_student="src/FileReader.java src/Student.java"
+javac -cp ${classpath} -d classes/ $file_student $hash_map
+java -cp $classpath MyHashMapTest
+# javac -cp ${classpath} -d classes/ src/ArrayListADT.java
+# javac -cp ${classpath} -d classes/ src/CircularArrayList.java
+# javac -cp ${classpath} -d classes/ src/CircularArrayListTest.jav
+# javac -cp ${classpath} -d classes/ src/DefaultMap.java
+# javac -cp ${classpath} -d classes/ src/FileReader.java
+# javac -cp ${classpath} -d classes/ src/MaxHeap.java
+# javac -cp ${classpath} -d classes/ src/MyHashMap.java
+# javac -cp ${classpath} -d classes/ src/MyHashMapTest.java
+# javac -cp ${classpath} -d classes/ src/Student.java
+
+
+# javac -cp ${classpath} -d classes/ src/ArrayListADT.java
+# javac -cp ${classpath} -d classes/ src/CircularArrayList.java
+# javac -cp ${classpath} -d classes/ src/CircularArrayListTest.java

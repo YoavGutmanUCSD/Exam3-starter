21f03f9dee2f1033916415cea1fe1d44b48915fe
YoavGutmanUCSD <ygutman@ucsd.edu>
2022-05-28 23:07:52 -0700

    Removed -1

--- src/CircularArrayListTest.java
+++ src/CircularArrayListTest.java
@@ -18,7 +18,7 @@ public class CircularArrayListTest {
         cal.addRear(7);
         cal.addRear(77);
         assertEquals(cal.get(0), (Integer) 70);
-        int diff = cal.getRear() + cal.getSize() - 1 - cal.getFront();
+        int diff = cal.getRear() + cal.getSize() - cal.getFront();
         System.out.format("front:%s\nrear:%s\ndiff:%s\n", cal.getFront(), cal.getRear(), cal.getFront());
         assertEquals(cal.get(cal.getRear()), (Integer) 77);
 

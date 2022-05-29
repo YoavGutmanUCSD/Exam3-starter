93f2d6395dfdab450682193a2dd4722349dcc1b6
YoavGutmanUCSD <ygutman@ucsd.edu>
2022-05-28 22:51:52 -0700

    First draft of CircularArrayList

--- src/CircularArrayList.java
+++ src/CircularArrayList.java
@@ -1,45 +1,95 @@
-
 public class CircularArrayList<T> implements ArrayListADT<T>{
-	
-	int capacity;
-	int size;
-	int front;
-	int rear;
-	T[] arrayList;
-	
-	public CircularArrayList() {
-		//Fill in the constructor for the circularArrayList
-	}
-	
-	public CircularArrayList(int initialCapacity) {
-		
-		//Fill in the constructor for the circularArrayList
-	}
 
-	@Override
-	public void addRear(T element) {
-		//Method to add element at the rear of the arraylist
-	}
+    int capacity;
+    int size;
+    int front;
+    int rear;
+    T[] arrayList;
 
-	@Override
-	public void addFront(T element) {
-		
-		//Method to add element at the front of the arraylist i.e. towards start
-		
-	}
+    public CircularArrayList() {
+        this(100);
+    }
 
-	public T get(int index) throws Exception {
-		
-		//Method to get element at a given index of the circularArrayList
-	}
-	
+    public CircularArrayList(int initialCapacity) {
+        this.size = 0;
+        this.capacity = initialCapacity;
+        this.front = 0;
+        this.rear = capacity-1;
+        this.arrayList = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void addRear(T element) {
+        //Method to add element at the rear of the arraylist
+        expandCapacity();
+        if(this.size == 0){
+            this.arrayList[this.rear] = element;
+        }
+        else {
+            this.rear = offset(rear, +1);
+            this.arrayList[this.rear] = element;
+        }
+        size++;
+    }
+
+    @Override
+    public void addFront(T element) {
+        //Method to add element at the front of the arraylist i.e. towards start
+        expandCapacity();
+        if(this.size == 0){
+            this.arrayList[this.front] = element;
+        }
+        else {
+            this.front = offset(front, -1);
+            this.arrayList[this.front] = element;
+        }
+        size++;
+    }
+
+    public T get(int index) throws Exception {
+        //Method to get element at a given index of the circularArrayList
+        // offset() should accomplish what is necessary here, though it's really REALLY weird.
+        return arrayList[offset(this.front, index)];
+    }
 
 
-	@Override
-	public T remove() {
-		
-		//Method to remove the element from the front
-	}
-	
-	
+
+    @Override
+    public T remove() {
+        T toReturn = this.arrayList[front];
+        this.arrayList[front] = null;
+        this.front = offset(front, +1);
+        return toReturn;
+    }
+    public int getSize(){
+        return this.size;
+    }
+    public int getRear(){
+        return this.rear;
+    }
+    public int getFront(){
+        return this.front;
+    }
+    public int getCapacity(){
+        return this.capacity;
+    }
+
+    private int offset(int startIndex, int offsetAmount){
+        return (this.arrayList.length + startIndex + offsetAmount) % this.arrayList.length;
+    }
+
+    private void expandCapacity(){
+        if(size != capacity) {
+            return;
+        }
+        this.capacity *= 2;
+        T[] newArrayList = (T[]) new Object[capacity];
+        for(int i = 0; i < this.front; i++){
+            newArrayList[i] = arrayList[i];
+        }
+        for(int i = this.size-1; i < this.rear; i--){
+            newArrayList[i] = arrayList[i];
+        }
+    }
+
 }

7623d367bb200a2a256d8a7935622e46788a6e38
YoavGutmanUCSD <ygutman@ucsd.edu>
2022-05-29 01:40:48 -0700

    The return of the test case

--- src/CircularArrayListTest.java
+++ src/CircularArrayListTest.java
@@ -4,41 +4,41 @@ import org.junit.*;
 public class CircularArrayListTest {
 
 
-    //// @Test
-    //public void test_baseCase() throws Exception {
+    @Test
+    public void test_baseCase() throws Exception {
 
-    //    // ArrayListADT<Integer> cal = new CircularArrayList<Integer> (10);
-    //    CircularArrayList<Integer> cal = new CircularArrayList<Integer> (10);
-    //    //Complete testcase to check elements at few positions
-    //    cal.addFront(7);
-    //    cal.addFront(7);
-    //    cal.addFront(7);
-    //    cal.addFront(70);
-    //    cal.addRear(7);
-    //    cal.addRear(7);
-    //    cal.addRear(7);
-    //    cal.addRear(77);
-    //    assertEquals(cal.get(0), (Integer) 70);
-    //    int diff = cal.getRear() + cal.getCapacity() - cal.getFront();
-    //    // System.out.format("front:%s\nrear:%s\ndiff:%s\n", cal.getFront(), cal.getRear(), cal.getFront());
-    //    // System.out.println(cal.asString());
-    //    assertEquals(cal.get(diff), (Integer) 77);
-    //}
-    //@Test
-    //public void test_Remove() throws Exception {
-    //    CircularArrayList<Integer> cal = new CircularArrayList<Integer> (10);
-    //    //Complete testcase to check elements at few positions
-    //    cal.addFront(7);
-    //    cal.addFront(7);
-    //    cal.addFront(7);
-    //    cal.addFront(70);
-    //    cal.addRear(7);
-    //    cal.addRear(7);
-    //    cal.addRear(7);
-    //    cal.addRear(77);
-    //    assertEquals(cal.remove(), (Integer) 70);
-    //    assertEquals(cal.get(0), (Integer) 7);
-    //}
+        // ArrayListADT<Integer> cal = new CircularArrayList<Integer> (10);
+        CircularArrayList<Integer> cal = new CircularArrayList<Integer> (10);
+        //Complete testcase to check elements at few positions
+        cal.addFront(7);
+        cal.addFront(7);
+        cal.addFront(7);
+        cal.addFront(70);
+        cal.addRear(7);
+        cal.addRear(7);
+        cal.addRear(7);
+        cal.addRear(77);
+        assertEquals(cal.get(0), (Integer) 70);
+        int diff = cal.getRear() + cal.getCapacity() - cal.getFront();
+        // System.out.format("front:%s\nrear:%s\ndiff:%s\n", cal.getFront(), cal.getRear(), cal.getFront());
+        // System.out.println(cal.asString());
+        assertEquals(cal.get(diff), (Integer) 77);
+    }
+    @Test
+    public void test_Remove() throws Exception {
+        CircularArrayList<Integer> cal = new CircularArrayList<Integer> (10);
+        //Complete testcase to check elements at few positions
+        cal.addFront(7);
+        cal.addFront(7);
+        cal.addFront(7);
+        cal.addFront(70);
+        cal.addRear(7);
+        cal.addRear(7);
+        cal.addRear(7);
+        cal.addRear(77);
+        assertEquals(cal.remove(), (Integer) 70);
+        assertEquals(cal.get(0), (Integer) 7);
+    }
     @Test
     public void test_expandCapacity() throws Exception {
         CircularArrayList<Integer> cal = new CircularArrayList<Integer> (10);

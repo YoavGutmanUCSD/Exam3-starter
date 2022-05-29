b0cc4dfa56f940f7e8064602b524a9294092372f
YoavGutmanUCSD <ygutman@ucsd.edu>
2022-05-29 01:07:24 -0700

    More prints!

--- src/CircularArrayList.java
+++ src/CircularArrayList.java
@@ -111,6 +111,7 @@ public class CircularArrayList<T> implements ArrayListADT<T>{
         T[] oldArrayList = this.arrayList;
         for(int i = this.front; i < oldArrayList.length; i++){
             int newIndex = this.front - oldArrayList.length + newArrayList.length;
+            System.out.format("Old index: %s. New index: %s.", i, newIndex);
             newArrayList[newIndex] = oldArrayList[i];
         }
         for(int i = 0; i <= this.rear; i++){

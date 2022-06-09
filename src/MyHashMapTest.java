ce4f5d698116766b37f0e71336844771d7d21834
YoavGutmanUCSD <ygutman@ucsd.edu>
2022-06-08 20:15:24 -0700

    Revert "FOR THE LOVE OF GOD"
    This reverts commit 4f455b1a52c15accc6d7e9a6fc09e2095a59ef08.

--- src/MyHashMapTest.java
+++ src/MyHashMapTest.java
@@ -12,78 +12,78 @@ public class MyHashMapTest {
 
     private FileReader filereader;
     private DefaultMap<Integer, Student> testMap; // use this for basic tests
-    // private MaxHeap<String, Integer> testHeap;
+    private MaxHeap<String, Integer> testHeap;
 
     @Before
-    public void setUp() {
+    public void initialize() {
         Comparator comp = new TestComp();
-        // this.testHeap = new MaxHeap<String, Integer>(15, comp);
+        this.testHeap = new MaxHeap<String, Integer>(15, comp);
         this.filereader = new FileReader("src/input.txt");
     }
 
-    // @Test
-    // public void testAddHeap(){
-    //     testHeap.add("a", 5);
-    //     testHeap.add("b", 7);
-    //     testHeap.add("c", 1);
-    //     assertEquals("b", testHeap.remove().getKey());
-    //     // testHeap.sanityCheck();
-    //     // assertEquals("[c, b, a]", testHeap.toString());
-    // }
+    @Test
+    public void testAddHeap(){
+        testHeap.add("a", 5);
+        testHeap.add("b", 7);
+        testHeap.add("c", 1);
+        assertEquals("b", testHeap.remove().getKey());
+        // testHeap.sanityCheck();
+        // assertEquals("[c, b, a]", testHeap.toString());
+    }
 
-    // @Test
-    // public void testAddHeapOutOfOrder(){
-    //     testHeap.add("b", 7);
-    //     testHeap.add("a", 5);
-    //     testHeap.add("c", 1);
-    //     // assertEquals("[c, b, a]", testHeap.toString());
-    //     assertEquals("b", testHeap.remove().getKey());
-    //     // testHeap.sanityCheck();
-    // }
+    @Test
+    public void testAddHeapOutOfOrder(){
+        testHeap.add("b", 7);
+        testHeap.add("a", 5);
+        testHeap.add("c", 1);
+        // assertEquals("[c, b, a]", testHeap.toString());
+        assertEquals("b", testHeap.remove().getKey());
+        // testHeap.sanityCheck();
+    }
 
-    // @Test
-    // public void testAddHeapMany(){
-    //     testHeap.add("b", 7);
-    //     testHeap.add("a", 5);
-    //     testHeap.add("c", 1);
-    //     testHeap.add("asetd", 5);
-    //     testHeap.add("d", 5);
-    //     testHeap.add("f", 5);
-    //     testHeap.add("e", 5);
-    //     testHeap.add("g", 5);
-    //     assertEquals("b", testHeap.remove().getKey());
-    //     // testHeap.sanityCheck();
-    //     // assertEquals("[g, f, asetd, e, a, b, c, d]", testHeap.toString());
-    // }
-    // @Test
-    // public void testAddHeapMaximumMiddle(){
-    //     testHeap.add("b", 7);
-    //     testHeap.add("a", 5);
-    //     testHeap.add("c", 1);
-    //     testHeap.add("z", 5);
-    //     testHeap.add("d", 5);
-    //     testHeap.add("f", 5);
-    //     testHeap.add("e", 5);
-    //     testHeap.add("g", 5);
-    //     assertEquals("b", testHeap.remove().getKey());
-    //     // assertTrue(testHeap.sanityCheck());
-    //     // assertEquals("[z, g, e, f, b, c, d]", testHeap.toString());
-    // }
-    // @Test
-    // public void testRemoveHeapMaximumMiddle(){
-    //     testHeap.add("b", 7);
-    //     testHeap.add("a", 5);
-    //     testHeap.add("c", 1);
-    //     testHeap.add("z", 5);
-    //     testHeap.add("d", 5);
-    //     testHeap.add("f", 5);
-    //     testHeap.add("e", 5);
-    //     testHeap.add("g", 5);
-    //     HeapEntry<String,Integer> removed = testHeap.remove();
-    //     System.out.println(testHeap.toString());
-    //     // assertTrue(testHeap.sanityCheck());
-    //     assertEquals(removed.getKey(), "b");
-    // }
+    @Test
+    public void testAddHeapMany(){
+        testHeap.add("b", 7);
+        testHeap.add("a", 5);
+        testHeap.add("c", 1);
+        testHeap.add("asetd", 5);
+        testHeap.add("d", 5);
+        testHeap.add("f", 5);
+        testHeap.add("e", 5);
+        testHeap.add("g", 5);
+        assertEquals("b", testHeap.remove().getKey());
+        // testHeap.sanityCheck();
+        // assertEquals("[g, f, asetd, e, a, b, c, d]", testHeap.toString());
+    }
+    @Test
+    public void testAddHeapMaximumMiddle(){
+        testHeap.add("b", 7);
+        testHeap.add("a", 5);
+        testHeap.add("c", 1);
+        testHeap.add("z", 5);
+        testHeap.add("d", 5);
+        testHeap.add("f", 5);
+        testHeap.add("e", 5);
+        testHeap.add("g", 5);
+        assertEquals("b", testHeap.remove().getKey());
+        // assertTrue(testHeap.sanityCheck());
+        // assertEquals("[z, g, e, f, b, c, d]", testHeap.toString());
+    }
+    @Test
+    public void testRemoveHeapMaximumMiddle(){
+        testHeap.add("b", 7);
+        testHeap.add("a", 5);
+        testHeap.add("c", 1);
+        testHeap.add("z", 5);
+        testHeap.add("d", 5);
+        testHeap.add("f", 5);
+        testHeap.add("e", 5);
+        testHeap.add("g", 5);
+        HeapEntry<String,Integer> removed = testHeap.remove();
+        System.out.println(testHeap.toString());
+        // assertTrue(testHeap.sanityCheck());
+        assertEquals(removed.getKey(), "b");
+    }
     //Write testcase for checking max score of 2 sections
     @Test
     public void testMaxInTwoSections() throws FileNotFoundException{
